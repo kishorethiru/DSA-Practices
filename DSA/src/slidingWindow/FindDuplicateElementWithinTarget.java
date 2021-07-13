101e1dc93df7012fa1ff144927d22328a11fec74
Kishore Kumar <kishorethiru10@gmail.com>
2021-07-13 19:09:05 +0530

    Updated

--- DSA/src/slidingWindow/FindDuplicateElementWithinTarget.java
+++ DSA/src/slidingWindow/FindDuplicateElementWithinTarget.java
@@ -45,10 +45,10 @@ public class FindDuplicateElementWithinTarget
 		        Validate with the interviewer if the data set is fine by his/ her assumptions
 		
 		3. Do I know to solve it?
-		        Yes- great is there an alternate ? Brute foce
+		        Yes- great is there an alternate ? Brute force
 		        No - can I break the problem into sub problems?
 		
-		4. Ask for hint (if you dont know how to solve this
+		4. Ask for hint (if you dont know how to solve this)
 		5.  Do I know alternate solutions as well
 		        Yes- what are those?
 		        No- that is still fine, proceed to solve by what you know
@@ -76,13 +76,13 @@ public class FindDuplicateElementWithinTarget
 	@Test
 	public void testData03() {
 		int[] data = { 1, 2, 3, 2, 2, 3 };
-		int target = 2;
+		int target = 3;
 		Assert.assertTrue(isDuplicateElementFound(data, target));
 	}
 	
 	@Test
 	public void testData04() {
-		int[] data = {8,5,4,18,3,1,8};
+		int[] data = {8,5,4,18,3,8};
 		int target = 6;
 		Assert.assertTrue(isDuplicateElementFound(data, target));
 	}
@@ -113,7 +113,8 @@ public class FindDuplicateElementWithinTarget
 //			}
 //			return false;
 //		}
-		
+	// 1, 2, 3,7,1,4,5,1
+		// T - 3	
 	private boolean twoDistinceIndicesPresent(int[] data, int target) {
 		int startIndex = 0, endIndex = 1;
 		while (endIndex <=data.length) { // Iterate the input from 1 to end
@@ -130,22 +131,20 @@ public class FindDuplicateElementWithinTarget
 		}
 		return false;
 	}
-	
+	 
 	/* If target value is > input size , return false
-	 * Create a hash set
+	 * Create a set
 	 * Iterate the input till the target value 
 	 * 		Add the items to set, if duplicate element found return true
 	 * Iterate the input after target to end
 	 *      Remove the first value from the set and add the next value
 	 *      if duplicate element found return true
-	 *      
 	 * return false
 	 * 
 	 * Time : O(n)
 	 * Space : O(n)
 	 * 
 	 */
-	
 	 private boolean isDuplicateElementFound(int[] data, int target)
 	 {
 		 if (target > data.length)
