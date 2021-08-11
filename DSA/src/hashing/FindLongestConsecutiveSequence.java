c872c6f96df7a4e1e47a7716bcf0bc2527e5d576
Kishore Kumar <kishorethiru10@gmail.com>
2021-08-11 23:39:48 +0530

    Updated

--- DSA/src/hashing/FindLongestConsecutiveSequence.java
+++ DSA/src/hashing/FindLongestConsecutiveSequence.java
@@ -11,7 +11,7 @@ public class FindLongestConsecutiveSequence {
 	/*
 	 *  
 	 * Given an unsorted array of integers nums, return the length of the 
-	 * longest consecutive elements sequence.You must write an algorithm that runs in O(n) time
+	 * longest consecutive elements sequence.You must write an algorithm that runs in O(n) time,
 	 */
 	
 	@Test
