388907b3f947e95b9522af9ab878a909d9724c46
kishorethiru <kishorethiru10@gmail.com>
2021-09-29 08:56:48 +0530

    Optimized solution for Decode String

--- DSA/src/stacks/P67_DecodeString.java
+++ DSA/src/stacks/P67_DecodeString.java
@@ -2,11 +2,7 @@ package stacks;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.Stack;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -55,54 +51,48 @@ public class P67_DecodeString {
 		String input = "2[abc2[ab2[c]]]";
 		Assert.assertTrue(decodeString(input).equals("abcabccabccabcabccabcc"));
 	}
-
-
-	/**
-	 * @param input
-	 * @return
+	
+	/*
+	 * Create a Stack
+	   Iterate the input and add the each char to Stack 
+ 		  If [ is found , add the items till [ is found to array List
+ 		  Pop '[' brace
+ 		  Initialize K as 1 
+  		  if is a digit iterate till [ or if char present or till stack is not empty
+          update K value
+          Push the items to stack k times
+		Create StringBuilder and pop the items from stack and return
+		Time : O(m*n) m - multiple digits and n - brackets
+	    Space : O(m*n) m - multiple digits and n - brackets
 	 */
-	/* 
-	 * Create a new stack
-	 * Iterate the input 
-	 * 		Push the values to the stack untill close bracket is found
-	 * 		else 
-	 *         Create a list and add the values to the list untill open bracket is found
-	 *         After open bracket, pop the next item and add to list 
-	 *         Iterate the list in reverse order for the next found k items 
-	 *         and push to the stack again 
-	 * return the stack as string
-	 * Time : O(m*n) m - multiple digits and n - brackets
-	 * Space : O(m*n) m - multiple digits and n - brackets
-	 */
-	private String decodeString(String input) {
+	private String decodeString(String s) {
 		Stack<Character> stack = new Stack<>();
-		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) != ']') stack.push(input.charAt(i));
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != ']')
+				stack.push(s.charAt(i));
 			else {
 				List<Character> list = new ArrayList<>();
-				while(stack.peek() != '[') {
-					list.add(stack.pop());				
-				}
+				while (stack.peek() != '[')
+					list.add(stack.pop());
 				stack.pop();
-				int k = 1;
-				String cal = "";
-				while(!stack.empty() && Character.isDigit(stack.peek())) 
-					cal += stack.pop();
-				k = Character.getNumericValue(stack.pop());
-				while(k > 0)
-				{
-					for (int j = list.size()-1; j >= 0 ; j--) {
-						stack.push(list.get(j));
+				int times = 1;
+				if (Character.isDigit(stack.peek())) {
+					StringBuilder valueOfK = new StringBuilder();
+					while (!stack.empty() && stack.peek() != '[' && Character.isDigit(stack.peek())) {
+						valueOfK.insert(0, stack.pop());
 					}
-					k--;
-				}			
+					times = Integer.valueOf(valueOfK.toString());
+				}
+				while (times > 0) {
+					for (int j = list.size() - 1; j >= 0; j--)
+						stack.push(list.get(j));
+					times--;
+				}
 			}
 		}
 		StringBuilder sb = new StringBuilder();
-		for (Character eachChar : stack) {
-			sb.append(eachChar);
-		}
+		for (Character c : stack)
+			sb.append(c);
 		return sb.toString();
 	}
-
 }
